bf62c256a55204e9bc7d8e0fa55ee77bb2e9a1cf
David Hutchison <dhutchis68516@gmail.com>
2020-03-27 18:44:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
 
-def now_serving
+def now_serving(person)
+  if person.count == 0
+    puts "There is nobody waiting to be served!"
 end
